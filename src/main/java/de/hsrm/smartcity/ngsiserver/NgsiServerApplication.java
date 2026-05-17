ff8e24bc28ddf48cee91fb6d8ed0d3b730cddcfc
Cassian <wwwcassianott@gmail.com>
2026-05-17 14:08:23 +0200

    NGSI-Server Application

---
+++ src/main/java/de/hsrm/smartcity/ngsiserver/NgsiServerApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.smartcity.ngsiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgsiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgsiServerApplication.class, args);
+	}
+
+}
